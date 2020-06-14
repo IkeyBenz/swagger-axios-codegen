e7cfce7cabb4b0cc428aeb9e8176c02b0230fe17
IkeyBenz <ikey.benz@gmail.com>
2020-06-14 13:21:57 -0400

    [remove] hardcoded repo path

--- generateApi.ts
+++ generateApi.ts
@@ -18,117 +18,117 @@ const getLineNumbersFromFileData = data => {
   return ret
 }
 
-// const PRETTIER_CONFIG = {
-//   bracketSpacing: true,
-//   jsxBracketSameLine: true,
-//   singleQuote: true,
-//   trailingComma: 'all',
-//   parser: 'typescript'
-// }
+const PRETTIER_CONFIG = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  parser: 'typescript'
+}
 
-// codegen({
-//   methodNameMode: 'summary',
-//   source: require('./swagger.json'),
-//   useCustomerRequestInstance: false,
-//   outputDir: './api2',
-//   multipleFileMode: true,
-//   useClassTransformer: false,
-//   useStaticMethod: true,
-//   serviceNameSuffix: '',
-//   extendGenericType: [],
-//   extendDefinitionFile: undefined,
-//   strictNullChecks: true,
-//   modelMode: 'interface',
-//   format: (text: string) => prettier.format(text, PRETTIER_CONFIG)
+codegen({
+  methodNameMode: 'summary',
+  source: require('./swagger.json'),
+  useCustomerRequestInstance: false,
+  outputDir: './api2',
+  multipleFileMode: true,
+  useClassTransformer: false,
+  useStaticMethod: true,
+  serviceNameSuffix: '',
+  extendGenericType: [],
+  extendDefinitionFile: undefined,
+  strictNullChecks: true,
+  modelMode: 'interface',
+  format: (text: string) => prettier.format(text, PRETTIER_CONFIG)
+})
+
+// const oldApiDir = hoverRepoDir + 'src/api2/'
+// const oldDecDir = hoverRepoDir + 'src/swagger.d.ts'
+
+// const newApiDir = './api2/'
+// const newDecDir = './swagger.d.ts'
+
+// let oldApiFiles = []
+// let newApiFiles = []
+
+// fs.readdirSync(oldApiDir).forEach(file => {
+//   oldApiFiles.push(file)
 // })
 
-const oldApiDir = hoverRepoDir + 'src/api2/'
-const oldDecDir = hoverRepoDir + 'src/swagger.d.ts'
+// fs.readdirSync(newApiDir).forEach(file => {
+//   newApiFiles.push(file)
+// })
 
-const newApiDir = './api2/'
-const newDecDir = './swagger.d.ts'
+// let sharedApiFiles = newApiFiles.filter(file => oldApiFiles.includes(file))
+// let newFiles = newApiFiles.filter(file => !oldApiFiles.includes(file))
+// let removedFiles = oldApiFiles.filter(file => !newApiFiles.includes(file))
 
-let oldApiFiles = []
-let newApiFiles = []
+// sharedApiFiles.forEach(file => {
+//   // Read New file
+//   fs.readFile(newApiDir + file, function read(err, data) {
+//     if (err) throw err
+//     const newData = data
+//     const newDataLineNumbers = getLineNumbersFromFileData(newData)
+//     // Read old file
+//     fs.readFile(oldApiDir + file, function read(err, data) {
+//       if (err) throw err
+//       const oldData = data
+//       const oldDataLineNumbers = getLineNumbersFromFileData(oldData)
 
-fs.readdirSync(oldApiDir).forEach(file => {
-  oldApiFiles.push(file)
-})
+//       let difference = diff.diffLines(oldData.toString(), newData.toString())
 
-fs.readdirSync(newApiDir).forEach(file => {
-  newApiFiles.push(file)
-})
+//       if (difference.length == 1) {
+//         process.stderr.write(`File `['grey'] + `.../${file}`['white'] + ` is unchanged\n`['grey'])
+//       } else {
+//         process.stderr.write(
+//           `\nFile `['grey'] +
+//             `.../${file}`['cyan'] +
+//             ` has `['grey'] +
+//             `${difference.length} ` +
+//             `differences\n`['grey']
+//         )
+//         process.stderr.write(`${outputSplit}\n`['grey'])
 
-let sharedApiFiles = newApiFiles.filter(file => oldApiFiles.includes(file))
-let newFiles = newApiFiles.filter(file => !oldApiFiles.includes(file))
-let removedFiles = oldApiFiles.filter(file => !newApiFiles.includes(file))
+//         difference.forEach(line => {
+//           let color = line.added ? 'green' : line.removed ? 'red' : 'grey'
+//           let plusMinus = line.added ? '+ ' : line.removed ? '- ' : ''
+//           let lineNumber = oldDataLineNumbers[line.value.split('\n')[0]]
+//           if (!lineNumber) lineNumber = newDataLineNumbers[line.value.split('\n')[0]]
+//           let offset = 5 - lineNumber.toString().length
+//           lineNumber = '  ' + lineNumber.toString()['grey'] + ' '.repeat(offset)
+//           let lineNumberOffset = '         '
+//           // console.log(line.value.split('\n')[0])
+//           if (color == 'grey') return
+//           let numLines = line.value.split('\n').length - 1
+//           let lineValue = line.value
+//           if (numLines > 1) {
+//             // lineValue = lineValue.replace('\n', '\n')
+//             // console.log(lineValue)
+//             let first = true
+//             lineValue.split('\n').forEach(line => {
+//               line += '\n'
+//               if (first) {
+//                 process.stderr.write(lineNumber + plusMinus + line[color])
+//                 first = false
+//               } else {
+//                 process.stderr.write(lineNumberOffset + line[color])
+//               }
+//             })
+//           } else {
+//             process.stderr.write(lineNumber + plusMinus + lineValue[color])
+//           }
+//         })
 
-sharedApiFiles.forEach(file => {
-  // Read New file
-  fs.readFile(newApiDir + file, function read(err, data) {
-    if (err) throw err
-    const newData = data
-    const newDataLineNumbers = getLineNumbersFromFileData(newData)
-    // Read old file
-    fs.readFile(oldApiDir + file, function read(err, data) {
-      if (err) throw err
-      const oldData = data
-      const oldDataLineNumbers = getLineNumbersFromFileData(oldData)
+//         process.stderr.write(`${outputSplit}\n\n`['grey'])
+//       }
+//     })
+//   })
+// })
 
-      let difference = diff.diffLines(oldData.toString(), newData.toString())
+// removedFiles.forEach(file => {
+//   process.stderr.write(`File `['grey'] + `.../${file}`['red'] + ` has been removed\n`['grey'])
+// })
 
-      if (difference.length == 1) {
-        process.stderr.write(`File `['grey'] + `.../${file}`['white'] + ` is unchanged\n`['grey'])
-      } else {
-        process.stderr.write(
-          `\nFile `['grey'] +
-            `.../${file}`['cyan'] +
-            ` has `['grey'] +
-            `${difference.length} ` +
-            `differences\n`['grey']
-        )
-        process.stderr.write(`${outputSplit}\n`['grey'])
-
-        difference.forEach(line => {
-          let color = line.added ? 'green' : line.removed ? 'red' : 'grey'
-          let plusMinus = line.added ? '+ ' : line.removed ? '- ' : ''
-          let lineNumber = oldDataLineNumbers[line.value.split('\n')[0]]
-          if (!lineNumber) lineNumber = newDataLineNumbers[line.value.split('\n')[0]]
-          let offset = 5 - lineNumber.toString().length
-          lineNumber = '  ' + lineNumber.toString()['grey'] + ' '.repeat(offset)
-          let lineNumberOffset = '         '
-          // console.log(line.value.split('\n')[0])
-          if (color == 'grey') return
-          let numLines = line.value.split('\n').length - 1
-          let lineValue = line.value
-          if (numLines > 1) {
-            // lineValue = lineValue.replace('\n', '\n')
-            // console.log(lineValue)
-            let first = true
-            lineValue.split('\n').forEach(line => {
-              line += '\n'
-              if (first) {
-                process.stderr.write(lineNumber + plusMinus + line[color])
-                first = false
-              } else {
-                process.stderr.write(lineNumberOffset + line[color])
-              }
-            })
-          } else {
-            process.stderr.write(lineNumber + plusMinus + lineValue[color])
-          }
-        })
-
-        process.stderr.write(`${outputSplit}\n\n`['grey'])
-      }
-    })
-  })
-})
-
-removedFiles.forEach(file => {
-  process.stderr.write(`File `['grey'] + `.../${file}`['red'] + ` has been removed\n`['grey'])
-})
-
-newFiles.forEach(file => {
-  process.stderr.write(`File `['grey'] + `.../${file}`['green'] + ` has been added\n`['grey'])
-})
+// newFiles.forEach(file => {
+//   process.stderr.write(`File `['grey'] + `.../${file}`['green'] + ` has been added\n`['grey'])
+// })
